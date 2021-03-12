0af04cccf4c81d982f27c5cb71cca5cf3e6a18f9
Moss <NA>
2021-03-12 15:37:17 -0500

    modifying slack notification and removing slice

--- send-jenkins-notification/slack-notification.go
+++ send-jenkins-notification/slack-notification.go
@@ -1,48 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 	"github.com/slack-go/slack"
 )
 
-func main() {
 
-    args := os.Args[1:]
-    fmt.Println(args)
+func main() {
+	args := os.Args[1:]
+	fmt.Println(args)
 
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	preText := "*Hello! Your Jenkins build has finished!*"
-	jenkinsURL := "*Build URL:* " + args[0]
-	buildResult  := "*" + args[1] + "*"
+	jenkinsURL := "*Build URL:* " + args[0] 
+	buildResult := "*" +  args[1] + "*"
 	buildNumber := "*" + args[2] + "*"
 	jobName := "*" + args[3] + "*"
 
-	if buildResult == "*SUCCESS*" {
+	if buildResult == "*SUCCESS*" { 
 		buildResult = buildResult + " :white_check_mark:"
 	} else {
 		buildResult = buildResult + " :x:"
 	}
 
-	dividerSection1 := slack.NewDividerBlock()
-	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
-	preTextField := slack.NewTextBlockObject("mrkdwn", preText + "\n\n", false, false)
+	dividerSection1 := slack.NewDividerBlock() 
+	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL 
+	preTextField := slack.NewTextBlockObject("mrkdwn", preText + "\n\n", false, false) 
 	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
-	
-	fieldSlice := make([]*slack.TextBlockObject, 0)
-	fieldSlice = append(fieldSlice, jenkinsBuildDetailsField)
-	
-	fieldsSection := slack.NewSectionBlock(nil, fieldSlice, nil)
+
+	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
 	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
-	
+
 	msg := slack.MsgOptionBlocks(
-		preTextSection, 
-		dividerSection1,
-		fieldsSection,
+			preTextSection,
+			dividerSection1,
+			jenkinsBuildDetailsSection,
 	)
+
 	_, _, _, err := api.SendMessage(
-		//channel or user ID that you want to send the message to
-		"C01ACEBRWUC" ,
+		"C01ACEBRWUC",
 		msg,
 	)
 
@@ -50,5 +47,4 @@ func main() {
 		fmt.Printf("%s\n", err)
 		return
 	}
-
 }
